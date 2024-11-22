5075108123256ff38359ebd067736aaf48c2cb19
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2024-11-22 14:06:54 +0530

    made the app responsive...

--- src/utills/constant.js
+++ src/utills/constant.js
@@ -1,4 +1,4 @@
-export const LOGO_URL = "https://cdn2.f-cdn.com/contestentries/45266/7346122/527b9844693b5_thumb900.jpg";
-export const CDN_URL  = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.776084900000015&lng=86.14463939999999&restaurantId=";
-export const RES_LIST = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.776084900000015&lng=86.14463939999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const LOGO_URL = process.env.REACT_APP_LOGO_URL;
+export const CDN_URL = process.env.REACT_APP_CDN_URL;
+export const MENU_API = process.env.REACT_APP_MENU_API;
+export const RES_LIST = process.env.REACT_APP_RES_LIST;
